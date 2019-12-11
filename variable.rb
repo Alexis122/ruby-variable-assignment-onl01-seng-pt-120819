30eb9b298705dc129d36af04ba1bf59e0acebf6b
Alexis Priore <alexispriore@gmail.com>
2019-12-11 19:49:25 +0000

    Automatically backed up by Learn

--- variable.rb
+++ variable.rb
@@ -1,2 +1,11 @@
  # Define a local variable called 'greeting',
  # give it the value of the string "Hello World"
+
+
+  it "defined a local variable called greeting and set it equal to 'Hello World'" do
+    greeting = "Hello World" ('./variable.rb', "greeting")
+
+    expect(greeting).to eq("Hello World")
+  end
+
+end
